3c5b03abfead5fde59aeae901917a47e00a00df1
Kieumy050802 <89453136+Kieumy050802@users.noreply.github.com>
2021-08-24 17:32:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,13 +5,13 @@ const CONFIG = {
     btnIntro: 'hihi <3',
     title: 'Em gì ơi, em đánh rơi người yêu này 😙',
     desc: 'Từ thích thích thành thành yêu yêu rồi thương thương ',
-    btnYes: 'Vẫn cứ là thích pé <33',
-    btnNo: 'Không, là yêu pé ùi:3',
+    btnYes: 'Em cá thương pé tró ạ <33',
+    btnNo: 'Em pé yêu pé nà :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu pé zạ <3',
     btnReply: 'Gửi cho pé  <3',
     reply: 'Yêu thì yêu mà không yêu thì thương nà <33333333',
     mess: 'chùi uiii  🥰  Yêu thương em pé nhắm ạ 😘😘',
     messDesc: 'Gặp lại pé bao đi ăn nhó ',
-    btnAccept: 'Okiiiii lun ạ <3',
+    btnAccept: 'Zạaaaaa thích qué  <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
